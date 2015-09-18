99e8c16f2b7ff92fc5ffa26f7948af23310797d8
andrepolischuk <andre.polischuk@gmail.com>
2015-09-18 11:56:25 +0300

    Fix capitalization with names

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 const regexp = /(:?\.\s?|^)([A-Za-z\u00C0-\u1FFF\u2800-\uFFFD])/gi;
-
-export default input => input
-  .toLowerCase()
-  .replace(regexp, match => match.toUpperCase());
+export default input => input.replace(regexp, match => match.toUpperCase());
--- test.js
+++ test.js
@@ -1,18 +1,12 @@
 import test from 'ava';
 import capitalizeSentence from './index';
 
-const origin = 'A sentence can include 12.1 words grouped meaningfully to express a statement, question, exclamation, request, command or suggestion. С точки зрения пунктуации, предложение как законченная единица речи оформляется в конце точкой, восклицательным или вопросительным знаками — или многоточием.';
+const origin = 'A sentence can include words grouped meaningfully to express a statement, question, exclamation, request, command or suggestion. С точки зрения пунктуации (§12.2), предложение как законченная единица речи оформляется в конце точкой, восклицательным или вопросительным знаками — или многоточием.';
 
-const lower = 'a sentence can include 12.1 words grouped meaningfully to express a statement, question, exclamation, request, command or suggestion. С точки зрения пунктуации, предложение как законченная единица речи оформляется в конце точкой, восклицательным или вопросительным знаками — или многоточием.';
-
-const upper = 'A SENTENCE CAN INCLUDE 12.1 WORDS GROUPED MEANINGFULLY TO EXPRESS A STATEMENT, QUESTION, EXCLAMATION, REQUEST, COMMAND OR SUGGESTION. С ТОЧКИ ЗРЕНИЯ ПУНКТУАЦИИ, ПРЕДЛОЖЕНИЕ КАК ЗАКОНЧЕННАЯ ЕДИНИЦА РЕЧИ ОФОРМЛЯЕТСЯ В КОНЦЕ ТОЧКОЙ, ВОСКЛИЦАТЕЛЬНЫМ ИЛИ ВОПРОСИТЕЛЬНЫМ ЗНАКАМИ — ИЛИ МНОГОТОЧИЕМ.';
-
-const mixed = 'a senTENCE Can include 12.1 words grouPED MEAningfully to expreSS A Statement, questiON, EXCLamation, reQuest, command or suggesTION. С тоЧКИ ЗРения пунктуаЦии, ПРЕдложениЕ как законченная единицА РЕЧи офорМЛЯЕТСЯ В КОНЦЕ ТОЧКОЙ, ВОСКЛИЦАТельным иЛи вопросительнЫМ ЗНАКами — или многоточием.';
+const lower = 'a sentence can include words grouped meaningfully to express a statement, question, exclamation, request, command or suggestion. с точки зрения пунктуации (§12.2), предложение как законченная единица речи оформляется в конце точкой, восклицательным или вопросительным знаками — или многоточием.';
 
 test('should capitalize each sentence', t => {
-  t.plan(4);
+  t.plan(2);
   t.true(capitalizeSentence(origin) === origin);
   t.true(capitalizeSentence(lower) === origin);
-  t.true(capitalizeSentence(upper) === origin);
-  t.true(capitalizeSentence(mixed) === origin);
 });
