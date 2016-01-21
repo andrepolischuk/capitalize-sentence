75c0b571967dd4aed1b7623149b990524f05528a
andrepolischuk <andre.polischuk@gmail.com>
2016-01-21 13:23:05 +0300

    Bump deps

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
 import test from 'ava';
-import capitalizeSentence from './index';
+import capitalizeSentence from './index.es5';
 
 const origin = 'A sentence can include words grouped meaningfully to express a statement, question, exclamation, request, command or suggestion. С точки зрения пунктуации (§12.2), предложение как законченная единица речи оформляется в конце точкой, восклицательным или вопросительным знаками — или многоточием.';
 
 const lower = 'a sentence can include words grouped meaningfully to express a statement, question, exclamation, request, command or suggestion. с точки зрения пунктуации (§12.2), предложение как законченная единица речи оформляется в конце точкой, восклицательным или вопросительным знаками — или многоточием.';
 
 test('should capitalize each sentence', t => {
-  t.plan(2);
-  t.true(capitalizeSentence(origin) === origin);
-  t.true(capitalizeSentence(lower) === origin);
+  t.is(capitalizeSentence(origin), origin);
+  t.is(capitalizeSentence(lower), origin);
 });
