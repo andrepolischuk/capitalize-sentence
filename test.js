548ba623b33586e57029ea28cbd69a84134ad6dc
andrepolischuk <andre.polischuk@gmail.com>
2016-01-21 13:28:03 +0300

    Tweaks

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
 import test from 'ava';
-import capitalizeSentence from './index.es5';
+import capitalize from './index.es5';
 
-const origin = 'A sentence can include words grouped meaningfully to express a statement, question, exclamation, request, command or suggestion. С точки зрения пунктуации (§12.2), предложение как законченная единица речи оформляется в конце точкой, восклицательным или вопросительным знаками — или многоточием.';
+const origin = `
+  A sentence can include words grouped meaningfully to express a statement,
+  question, exclamation, request, command or suggestion. С точки зрения
+  пунктуации (§12.2), предложение как законченная единица речи оформляется
+  в конце точкой, восклицательным или вопросительным знаками — или многоточием.
+`.trim();
 
-const lower = 'a sentence can include words grouped meaningfully to express a statement, question, exclamation, request, command or suggestion. с точки зрения пунктуации (§12.2), предложение как законченная единица речи оформляется в конце точкой, восклицательным или вопросительным знаками — или многоточием.';
+const lower = `
+  a sentence can include words grouped meaningfully to express a statement,
+  question, exclamation, request, command or suggestion. с точки зрения
+  пунктуации (§12.2), предложение как законченная единица речи оформляется
+  в конце точкой, восклицательным или вопросительным знаками — или многоточием.
+`.trim();
 
-test('should capitalize each sentence', t => {
-  t.is(capitalizeSentence(origin), origin);
-  t.is(capitalizeSentence(lower), origin);
+test(t => {
+  t.is(capitalize(origin), origin);
+  t.is(capitalize(lower), origin);
 });
